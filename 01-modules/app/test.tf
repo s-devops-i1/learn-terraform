3ae55db01884a1a3b03aa80be8cc4d1f002ff658
shuja <shuja.hafez@gmail.com>
2024-05-28 15:28:13 +0530

    id changed to name

--- 01-modules/app/test.tf
+++ 01-modules/app/test.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "instance" {
   ami                    = "ami-090252cbe067a9e58"
   instance_type          = var.instance_type
-  vpc_security_group_ids = [data.aws_security_group.selected.id]
+  vpc_security_group_ids = [data.aws_security_group.selected.name]
 
   tags = {
     Name = var.Name
