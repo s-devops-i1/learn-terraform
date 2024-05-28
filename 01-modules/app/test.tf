e2c5b70854182268d3797e11879a44fb2657627c
shuja <shuja.hafez@gmail.com>
2024-05-28 14:36:18 +0530

    3 instance added

--- 01-modules/app/test.tf
+++ 01-modules/app/test.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "instance" {
-  ami                     = "ami-090252cbe067a9e58"
-  instance_type           = var.instance_type
-  vpc_security_group_ids  =  [data.aws_security_group.selected.id]
+  ami                    = "ami-090252cbe067a9e58"
+  instance_type          = var.instance_type
+  vpc_security_group_ids = [data.aws_security_group.selected.id]
 }
+
 
--- main.tf
+++ main.tf
@@ -1,4 +1,14 @@
-module "test-1" {
+module "frontend" {
+  source = "./01-modules/app"
+  instance_type = var.instance_type
+  security_group_id = var.security_group_id
+}
+module "backend" {
+  source = "./01-modules/app"
+  instance_type = var.instance_type
+  security_group_id = var.security_group_id
+}
+module "mysql" {
   source = "./01-modules/app"
   instance_type = var.instance_type
   security_group_id = var.security_group_id
