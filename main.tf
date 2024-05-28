e8abf3eec2e7e6a352203b533400ef51e8204824
shuja <shuja.hafez@gmail.com>
2024-05-28 15:24:59 +0530

    demo on tags

--- main.tf
+++ main.tf
@@ -1,21 +1,21 @@
 module "frontend" {
   source = "./01-modules/app"
-  instance_type = "t3.micro"
-  security_group_id = "sg-08f41a3b66746e56a"
-  Name              =  "frontend"
+  instance_type = var.instance_type
+  security_group_id = var.security_group_id
+  Name              =  var.Name
 
 
 }
-module "backend" {
-  source = "./01-modules/app"
-  instance_type = "t3.micro"
-  security_group_id = "sg-08f41a3b66746e56a"
-  Name              = "backend"
-}
-module "mysql" {
-  source = "./01-modules/app"
-  instance_type = "t3.micro"
-  security_group_id = "sg-08f41a3b66746e56a"
-  Name              = "mysql"
-}
+# module "backend" {
+#   source = "./01-modules/app"
+#   instance_type = "t3.micro"
+#   security_group_id = "sg-08f41a3b66746e56a"
+#   Name              = "backend"
+# }
+# module "mysql" {
+#   source = "./01-modules/app"
+#   instance_type = "t3.micro"
+#   security_group_id = "sg-08f41a3b66746e56a"
+#   Name              = "mysql"
+# }
 
---
+++ main.tfvars
@@ -0,0 +1,3 @@
+instance_type = "t3.micro"
+security_group_id = ""
+Name ="sg-08f41a3b66746e56a"
