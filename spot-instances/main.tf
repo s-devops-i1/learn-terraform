e3b42d86d9c87028170075fce3453c021cbb89b5
shuja <shuja.hafez@gmail.com>
2024-06-05 18:04:56 +0530

    market_type placed under different block

--- spot-instances/main.tf
+++ spot-instances/main.tf
@@ -7,13 +7,14 @@ resource "aws_instance" "instance" {
   }
   instance_market_options {
     market_type = "spot"
-
-  }
     spot_options {
 
       instance_interruption_behavior  =  "stop"
       spot_instance_type              =  "persistent"
     }
+
+
+  }
 
 
 }
