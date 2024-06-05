765ff355365f66b674e703ebb5c34797b2b22166
shuja <shuja.hafez@gmail.com>
2024-06-05 18:03:45 +0530

    market_type placed under different block

--- spot-instances/main.tf
+++ spot-instances/main.tf
@@ -1,8 +1,13 @@
 resource "aws_instance" "instance" {
   ami = "ami-090252cbe067a9e58"
   vpc_security_group_ids = ["sg-08f41a3b66746e56a"]
+  instance_type = "t3.small"
+  tags = {
+    Name = "test-spot"
+  }
   instance_market_options {
     market_type = "spot"
+
   }
     spot_options {
 
@@ -10,8 +15,5 @@ resource "aws_instance" "instance" {
       spot_instance_type              =  "persistent"
     }
 
-  instance_type = "t3.small"
-  tags = {
-    Name = "test-spot"
-  }
+
 }
